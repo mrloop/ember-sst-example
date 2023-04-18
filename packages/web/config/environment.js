3143883437bede8d006d3f02d232b793456bd123
mrloop <ewan@mrloop.com>
2023-04-18 22:16:54 +0100

    Config Ember.js app to use API

--- packages/web/config/environment.js
+++ packages/web/config/environment.js
@@ -18,6 +18,7 @@ module.exports = function (environment) {
       // Here you can pass flags/options to your application instance
       // when it is created
     },
+    apiUrl: process.env.EMBER_APP_API_URL,
   };
 
   if (environment === 'development') {
